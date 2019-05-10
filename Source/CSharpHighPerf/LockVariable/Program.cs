c54e130eba8e00d717b6b0a0756c38fe0d01ca96
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2019-05-10 08:32:43 -0500

    Lock variable demos

---
+++ Source/CSharpHighPerf/LockVariable/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LockVariable
+{
+    /// <summary>
+    /// Lock variable demos
+    /// </summary>
+    class Program
+    {
+        static void LockDemo()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Restart();
+            object syncObj = new object();
+            var masterList = new List<long>();
+            const int numTasks = 8;
+            Task[] tasks = new Task[numTasks];
+
+            for (int i = 0; i < numTasks; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    for (int j = 0; j < 10000000; j++)
+                    {
+                        lock (syncObj)
+                        {
+                            masterList.Add(j);
+                        }
+
+                    }
+                });
+            }
+            Task.WaitAll(tasks);
+            watch.Stop();
+            Console.WriteLine("Elapsed locked {0}: ", watch.Elapsed);
+        }
+
+
+        static void BetterLockDemo()
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Restart();
+            object syncObj = new object();
+            var masterList = new List<long>();
+            const int numTasks = 8;
+            Task[] tasks = new Task[numTasks];
+
+            for (int i = 0; i < numTasks; i++)
+            {
+                tasks[i] = Task.Run(() =>
+                {
+                    var localList = new List<long>();
+
+                    for (int j = 0; j < 10000000; j++)
+                    {
+                        localList.Add(j);
+
+                    }
+                    lock (syncObj)
+                    {
+                        masterList.AddRange(localList);
+                    }
+                });
+            }
+            Task.WaitAll(tasks);
+            watch.Stop();
+            Console.WriteLine("Elapsed beter locked {0}: ", watch.Elapsed);
+        }
+
+        static void Main(string[] args)
+        {
+            LockDemo();
+            BetterLockDemo();
+        }
+    }
+}

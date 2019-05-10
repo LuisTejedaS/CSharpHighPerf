84ceee510ae93e0c6a19bbd7faae2fab2b5c6001
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2019-05-10 08:49:15 -0500

    For and For each

---
+++ Source/CSharpHighPerf/ForAndForEach/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForAndForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10000000];
+            int sum = 0;
+
+            Stopwatch watch = new Stopwatch();
+            watch.Restart();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += i;
+            }
+
+            watch.Stop();
+            Console.WriteLine("Finished for, elapsed {0}", watch.Elapsed);
+
+            watch.Restart();
+
+            foreach (int val in arr)
+            {
+                sum += val;
+            }
+            watch.Stop();
+            Console.WriteLine("Finished for each, elapsed {0}", watch.Elapsed);
+
+
+            IEnumerable<int> arreNum = arr;
+            watch.Restart();
+
+            foreach (int val in arreNum)
+            {
+                sum += val;
+            }
+
+            watch.Stop();
+            Console.WriteLine("Finished for each ienum, elapsed {0}", watch.Elapsed);
+            Console.ReadKey();
+        }
+    }
+}

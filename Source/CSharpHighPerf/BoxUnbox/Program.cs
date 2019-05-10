37d06bddd7e95e7cae5720b95503661c11b70a13
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2019-05-10 08:40:39 -0500

    BoxUnbox

---
+++ Source/CSharpHighPerf/BoxUnbox/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace BoxUnbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int val = 13;
+            object boxedVal = val;
+            val = 14;
+            string.Format("val: {0}, boxedVal: {1}", val, boxedVal);
+            string.Format("Number of processes on machin: {0}", Process.GetProcesses().Length);
+        }
+    }
+}

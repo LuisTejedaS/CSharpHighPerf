f2cfc4765a0b89916aa4057e2f2d3a965a4ee9cf
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2019-05-10 08:34:21 -0500

    Volatile sample

---
+++ Source/CSharpHighPerf/VolatileSample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading; 
+
+namespace VolatileSample
+{
+    /// <summary>
+    /// demonstrate how to read a variable value with volatile
+    /// </summary>
+    class Program
+    {
+        volatile bool _loop = true;
+
+        private static void SomeThread(object o1)
+        {
+            Program o = (Program)o1;
+            Console.WriteLine("loop starting ...");
+            while (o._loop)
+            {
+
+            }
+            Console.WriteLine("loop stoping ..");
+        }
+
+        static void Main(string[] args)
+        {
+            Program o1 = new Program();
+            Thread t1 = new Thread(SomeThread);
+            t1.Start(o1);
+            Thread.Sleep(2000);
+            o1._loop = false;
+            Console.WriteLine("value set to false");
+        }
+    }
+}

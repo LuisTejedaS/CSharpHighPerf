22e50971c4617f59f027b11fc2a47184e455f90e
Luis Tejeda <46000487+LuisTejedaS@users.noreply.github.com>
2019-05-10 08:38:43 -0500

    Thread static

---
+++ Source/CSharpHighPerf/LightResource/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LightResource
+{
+    class Program
+    {
+        private static readonly ThreadLocal<Random> threadLocalRand = new ThreadLocal<Random>(() => new Random());
+
+        [ThreadStatic]
+        static Random threadStaticRand;
+        static void Main(string[] args)
+        {
+            Stopwatch watch = new Stopwatch();
+
+
+            watch.Restart();
+            int[] results = new int[100];
+            Console.WriteLine("ThreadLocal version");
+
+            Parallel.For(0, 500000000,
+                i =>
+                {
+                    var randomNumber = threadLocalRand.Value.Next(100);
+                    Interlocked.Increment(ref results[randomNumber]);
+                });
+            watch.Stop();
+            Console.WriteLine("elapsed time: {0}", watch.Elapsed);
+
+            watch.Restart();
+
+            Console.WriteLine("ThreadStatic version");
+
+            Parallel.For(0, 500000000,
+                i =>
+                {
+                    if (threadStaticRand == null)
+                    {
+                        threadStaticRand = new Random();
+                        var randomNumber = threadLocalRand.Value.Next(100);
+                        Interlocked.Increment(ref results[randomNumber]);
+                    }
+
+
+                });
+            watch.Stop();
+            Console.WriteLine("elapsed time: {0}", watch.Elapsed);
+
+
+
+        }
+    }
+}
